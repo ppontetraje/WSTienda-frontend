09caa5083b3b4ad3f2c878880e9ed3105b5758f1
Jean Piert Palomino Quispe <dragon3706@hotmail.com>
2021-04-24 16:07:41 -0500

    Conexión con backend

---
+++ src/app/services/apicliente.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiclienteService {
+
+  url: string = 'https://localhost:44357/api/cliente';
+
+
+  constructor(private http: HttpClient) { 
+    
+  }
+  getClientes(): Observable<Response> {
+
+    return this.http.get<Response>(this.url);
+
+  }
+}
+
